14d08e5ea18f49c10c83ec6c88a716d2b64aa416
Rahul Tiwari <rahultwr0005@gmail.com>
2022-03-07 21:33:46 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import AppContainer from './AppContainer';
+import Header from './Header';
 import logo from './logo.svg';
-import './App.css';
+
+const formFields = [
+    { id: 1, label: "First Name", type: "text"},
+    { id: 2, label: "Last Name", type: "text"},
+    { id: 3, label: "Email", type: "email"},
+    { id: 4, label: "Date of Birth", type: "date"}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AppContainer>
+            <div className="mx-auto p-5 bg-white rounded-xl shadow-lg">
+                <Header
+                    title={"Welcome to react-typescript with tailwindcss course"}
+                />
+                {formFields.map(field => (
+                    <React.Fragment key={field.id}>
+                        <label className="block text-gray-600 text-lg">
+                            {field.label}
+                        </label>
+                        <input 
+                            type={field.type} 
+                            className="w-full p-2 border-2 border-gray-200 rounded-lg mb-2"
+                        />
+                    </React.Fragment>
+                ))}
+                <button 
+                    className="w-3/12 px-6 py-2 bg-blue-600 rounded-lg text-white font-semibold text-lg mt-3"
+                >
+                    Submit
+                </button>
+            </div>
+        </AppContainer>
+    );
 }
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default function Header(props: {title: string}) {
+    return (
+        <div className="flex gap-2 items-center">
+            <img 
+                src={logo}
+                className="w-16 h-16" 
+                style={{ animation: "spin 2s linear infintie" }} 
+                alt="logo" 
+            />
+            <h2 className='text-xl text-center font-semibold'>{ props.title }</h2>
+        </div>
+    );
+}
