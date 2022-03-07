84d506f0d324797398a720a4fc3e5866b7a41c80
Rahul Tiwari <rahultwr0005@gmail.com>
2022-03-07 21:43:35 +0530

    added phone number field in form

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ const formFields = [
     { id: 1, label: "First Name", type: "text"},
     { id: 2, label: "Last Name", type: "text"},
     { id: 3, label: "Email", type: "email"},
-    { id: 4, label: "Date of Birth", type: "date"}
+    { id: 4, label: "Phone Number", type: "tel"},
+    { id: 5, label: "Date of Birth", type: "date"}
 ]
 
 function App() {
