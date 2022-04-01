8fd6351cf0f8d514f014d243659f46f949643e95
Rahul Tiwari <rahultwr0005@gmail.com>
2022-04-01 17:42:56 +0530

    removed unnecessary function

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -4,17 +4,11 @@ import { User } from './functions/types/User';
 
 export default function AppContainer(props: { user: User, children: React.ReactNode }) {
 
-    const childrenWithProps = React.Children.map(props.children, child => {
-        if(React.isValidElement(child)) {
-            return React.cloneElement(child, {user: props.user});
-        }
-        return child;
-    })
     return (
         <div className="flex h-screen items-center bg-gray-100">
             <div className="mx-auto w-3/4 max-w-[700px] p-5 bg-white rounded-xl shadow-lg">
                 <Header user={props.user} />
-                {childrenWithProps}
+                {props.children}
             </div>
         </div>
     )
